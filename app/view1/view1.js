fbd90723aa94dcd23ebe6be6430b7902deec59a1
fabio@fabiofarias.com.br <Pernalonga.12>
2016-11-30 23:44:33 -0200

    Primeira versao

---
+++ app/view1/view1.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var app = angular.module('myApp.view1', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+      });
+}]);
+
+app.controller('View1Ctrl', function($scope) {
+    var data = [
+      {
+        "city": "Rio de Janeiro",
+        "email": "fabio@fabiofarias.com.br"
+      },
+      {
+        "city": "Sao Paulo",
+        "email": "joao@fabiofarias.com.br"
+      },
+      {
+        "city": "Petrolina",
+        "email": "pedro@fabiofarias.com.br"
+      },
+      {
+        "city": "Fortaleza",
+        "email": "ricardo@fabiofarias.com.br"
+      },
+      {
+        "city": "Porto Alegre",
+        "email": "gabriela@fabiofarias.com.br"
+      },
+    ];
+
+    $scope.data = data;
+    $scope.search = "";
+    var type;
+
+    $scope.filter = function (value, index, array) {
+        var result = false;
+        switch (type){
+            case 'any':
+
+
+
+                break;
+            case 'city':
+              break;
+            case 'email':
+              break;
+        }
+    };
+
+});
